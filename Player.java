2631b8d93d93d0e8a68e0ed2c711c808c4f1795f
Manulji <79298095+Manulji@users.noreply.github.com>
2021-07-09 23:12:28 +0530

    Update Player.java

--- Player.java
+++ Player.java
@@ -8,7 +8,6 @@ public class Player {
     public String PlayerType;
     private static ArrayList<Player> players1 = new ArrayList<Player>();
 
-
     public Player(String name, int age, String playerType, int runs, Integer wickets) {
         this.setName(name);
         this.setAge(age);
@@ -19,7 +18,6 @@ public class Player {
 
     public void setName(String name) {
         this.name = name;
-
     }
     public String getName() {
         return name;
@@ -30,11 +28,10 @@ public class Player {
     public int getAge() {
         return age;
     }
-
     public double battingAvg(){
         double battingAvg = runs / 15.0;
-        return battingAvg;}
-
+        return battingAvg;
+    }
     public double bowlingAvg() {
         double bowlingAvg = wickets / 15.0;
         return bowlingAvg;
@@ -47,35 +44,27 @@ public class Player {
             System.out.println("No. of runs: "+ getRuns());
     }
 
-
     public void setPlayertype(String player_Type) {
         this.PlayerType= player_Type;
     }
-
     public String getPlayerType() {
 
        return this.PlayerType;
     }
-
     public int setRuns(int runs) {
        return this.runs = runs;
     }
-
     public int getRuns() {
         return runs;
     }
-
     public int setWickets(int wickets) {
         return this.wickets = wickets;
     }
-
     public int getWickets() {
         return wickets;
     }
-
     public void playercall(Player P){
         players1.add(P);
-
     }
     public ArrayList<Player> getPlayers1(){
         return players1;
